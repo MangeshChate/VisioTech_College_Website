0fd5e82dfa9285434bfdf2d49dd5f3c9b878b481
mangesh <mbchate08@gmail.com>
2024-02-29 01:04:32 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // Add the following lines
+  base: '/my-app/',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true, // Ensure the output directory is cleared before each build
+  },
+});
